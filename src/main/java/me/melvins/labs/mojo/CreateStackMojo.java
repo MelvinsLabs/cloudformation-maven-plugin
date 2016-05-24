4f7801e3c817c675857b8bdf184834e8f39a2efd
Melvins <melvinsonline+github@gmail.com>
2016-05-23 23:21:42 -0700

    Exposing Confiuration Parameters

--- src/main/java/me/melvins/labs/mojo/CreateStackMojo.java
+++ src/main/java/me/melvins/labs/mojo/CreateStackMojo.java
@@ -1,8 +1,16 @@
+/*
+ *
+ */
+
 package me.melvins.labs.mojo;
 
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
 import com.amazonaws.services.cloudformation.model.CreateStackRequest;
+import com.amazonaws.services.cloudformation.model.Tag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.MessageFormatMessageFactory;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -10,25 +18,58 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Created by Melvin_Mathai on 5/23/2016.
+ * @author Mels
  */
 @Mojo(name = "CreateStack", defaultPhase = LifecyclePhase.DEPLOY)
 public class CreateStackMojo extends AbstractMojo {
 
-    @Parameter
+    private static final Logger LOGGER = LogManager.getLogger(CreateStackMojo.class, new MessageFormatMessageFactory());
+
+    @Parameter(required = true)
+    private String stackName;
+
+    @Parameter(required = true)
     private String templateUrl;
+
+    @Parameter
+    private Map<String, String> tags;
+
+    @Override
+    public String toString() {
+        return "Configuration{" +
+                "stackName='" + stackName + '\'' +
+                ", templateUrl='" + templateUrl + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("Executing CreateStack Goal");
-        System.out.println("Template " + templateUrl);
+        LOGGER.info("Executing Goal: CreateStack");
+        LOGGER.info(toString());
 
         CreateStackRequest createStackRequest = new CreateStackRequest();
+        createStackRequest.setStackName(stackName);
+        createStackRequest.setTags(this.createTags());
         createStackRequest.setTemplateURL(templateUrl);
 
         AmazonCloudFormationClient amazonCloudFormationClient = new AmazonCloudFormationClient(new InstanceProfileCredentialsProvider());
         amazonCloudFormationClient.createStack(createStackRequest);
+    }
+
+    private List<Tag> createTags() {
+        List<Tag> awsTags = new ArrayList<>();
+
+        tags.forEach((k, v) -> {
+            awsTags.add(new Tag().withKey(k).withValue(v));
+        });
+
+        return awsTags;
     }
 
 }
